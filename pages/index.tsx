7c7db29b73725d7dd738a2ff0efc4e24905193e6
Elves Sousa <elves.sousa@gmail.com>
2021-04-02 19:43:59 -0300

    About section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Layout } from '../components/Layout';
 import { Palette } from '../components/Palette';
 import { Languages } from '../components/Languages';
-import { Screens } from '../components/Screens';
 import { AppsSupported } from '../components/AppSupported';
 
 export default function Home() {
@@ -12,33 +11,38 @@ export default function Home() {
       description="A theme with sober colors, hence the name"
       heroButtons
     >
+      <section className="about">
+        <h2 id="about">About</h2>
+        <p>
+          Sobrio is theme made to create a comfortable experience while coding.
+          I made it because I was a little tired of the purple/magenta colored
+          themes that seem to be the hype today. Nothing against it, but it is
+          not my type.
+        </p>
+        <p>
+          It's been actively developed: more apps supported and variants to come
+          soon. For those who like it white, a light theme is being made. Just
+          you wait!
+        </p>
+      </section>
+      <hr />
+
       <section className="available">
-        <h2>Available for</h2>
+        <h2 id="apps">Available for</h2>
         <AppsSupported />
       </section>
       <hr />
 
-      <section className="colors">
+      <section id="palette" className="colors">
         <h2>Palette</h2>
         <Palette />
       </section>
       <hr />
 
-      <section className="languages">
+      <section id="languages" className="languages">
         <h2>Languages supported</h2>
         <Languages />
       </section>
-
-      {/*<section className="screenshots">
-        <h2>Screenshots</h2>
-        <Screens />
-      </section>
-      <hr />
-
-      <section className="trial">
-        <h2>Test the theme</h2>
-        <p>Soon...</p>
-        </section> */}
     </Layout>
   );
 }
