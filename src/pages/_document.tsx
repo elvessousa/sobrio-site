8a2d3f4e08c6c1ebaa84f0fb01da3f39a01d55f2
Elves Sousa <elves.sousa@gmail.com>
2021-09-10 20:21:10 -0300

    Changing font url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="icon.png" type="image/png" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap"
             rel="stylesheet"
